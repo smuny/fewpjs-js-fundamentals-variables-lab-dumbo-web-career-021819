24a39230213eb440374829d6a54b0fec8aa313f7
github username <github email address>
2019-04-01 12:49:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Code your solution in this file!
+const companyName = 'Scuber';
+
+// 'let' can be reassigned but 'const' cant
+
+let mostProfitableNeighborhood = 'Chelsea'
+let companyCeo = 'Susan Smith'
